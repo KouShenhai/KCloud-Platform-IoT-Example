93020c555305951d184163942e95be599dd19741
koushenhai <2413176044@qq.com>
2024-05-02 11:32:43 +0800

    feat: 初始化提交

---
+++ iot-flyway-postgresql/src/main/java/org/laokou/flyway/FlywayPostgresqlApp.java
@@ -0,0 +1,13 @@
+package org.laokou.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPostgresqlApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayPostgresqlApp.class, args);
+    }
+
+}
