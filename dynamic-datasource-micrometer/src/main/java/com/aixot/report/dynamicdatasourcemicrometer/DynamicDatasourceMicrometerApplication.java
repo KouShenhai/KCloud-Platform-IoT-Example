5169d7cd838483c242c1cca9e48cb4df9c19d5be
koushenhai <2413176044@qq.com>
2024-08-21 14:04:08 +0800

    111

---
+++ dynamic-datasource-micrometer/src/main/java/com/aixot/report/dynamicdatasourcemicrometer/DynamicDatasourceMicrometerApplication.java
@@ -0,0 +1,13 @@
+package com.aixot.report.dynamicdatasourcemicrometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicDatasourceMicrometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicDatasourceMicrometerApplication.class, args);
+	}
+
+}
---
+++ dynamic-datasource-micrometer/src/test/java/com/aixot/report/dynamicdatasourcemicrometer/DynamicDatasourceMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aixot.report.dynamicdatasourcemicrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDatasourceMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
